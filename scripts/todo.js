897e7aacee96355ab62192b3c86611b2d810552b
gitbike <git_bike_840@yahoo.co.jp>
2019-02-08 19:16:01 +0900

    Descriptionの追加

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,7 +1,16 @@
+// Description:
+//  TODOを管理することが出来るボットです。
+// Commands:
+//  ボット名 todo - TODOを作成
+//  ボット名 done - TODOを完了にする
+//  ボット名 del - TODOを消す
+//  ボット名 list - TODOを一覧表示
+//  ボット名 donelist - 完了したTODOの一覧表示
+
 'use strict';
 
 const todo = require('todo');
 console.log(todo.list());
 module.exports = (robot) => {
-  
+
 };
